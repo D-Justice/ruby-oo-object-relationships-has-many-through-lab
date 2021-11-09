e7ddc66c071c68373ec3f9164d729f8726b90d1f
D-Justice <justice.damian96@gmail.com>
2021-11-10 08:42:13 +1100

    Assignment Complete

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|each| each.genre == self}
+    end
+    def artists
+        songs.collect {|each| each.artist}
+    end
+
+end
